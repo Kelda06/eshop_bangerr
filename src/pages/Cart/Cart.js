e8b2c9c71e6a2eac945bb228783988b21e491056
Adam Keller <114884755+Kelda06@users.noreply.github.com>
2024-04-23 13:59:01 +0200

    Delete src directory

--- src/pages/Cart/Cart.js
+++
@@ -1,8 +0,0 @@
-function updateTotal() {
-    var productPrice = parseInt(document.getElementById('productPrice').textContent);
-    var shippingPrice = parseInt(document.getElementById('shippingMethod').value);
-    var total = productPrice + shippingPrice;
-
-    document.getElementById('subtotal').textContent = productPrice;
-    document.getElementById('total').textContent = total;
-}
