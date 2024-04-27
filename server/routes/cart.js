209aaa03ece944f3256d36c41ad5e2c12620d759
Adam Keller <114884755+Kelda06@users.noreply.github.com>
2024-04-27 16:05:19 +0200

    Delete server/routes directory

--- server/routes/cart.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const Cart = require("../models/cart");
-const Item = require("../models/item");
-const Auth = require("../middleware/auth");
-const router = new express.Router();
